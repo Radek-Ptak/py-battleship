ff6399c0aac7a1200b4d605c54d65c63b4c20ead
Radoslaw Ptak <radek1026@interi.pl>
2025-10-01 15:43:24 +0200

    CR change in implement py-battleship

--- app/main.py
+++ app/main.py
@@ -50,8 +50,12 @@ class Ship:
     def fire(self, row: int, column: int) -> str:
         deck = self.get_deck(row, column)
 
-        if deck is None or not deck.is_alive:
+        if deck is None:
             return "Miss!"
+        elif not deck.is_alive:
+            if self.is_drowned:
+                return "Sunk!"
+            return "Hit!"
         deck.is_alive = False
         self.is_drowned = all(not d.is_alive for d in self.decks)
         if self.is_drowned:
